b405359a8902659c5b50e97d2c08ac769925a88f
vodaxq-png <vodaxq@gmail.com>
2025-10-20 00:24:49 +0300

    Create order_model.dart

---
+++ lib/order_model.dart
@@ -0,0 +1,13 @@
+class Order {
+  final String id;
+  final String name;
+  final double lat;
+  final double lng;
+
+  Order({
+    required this.id,
+    required this.name,
+    required this.lat,
+    required this.lng,
+  });
+}
